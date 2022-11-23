d62ebac5d37c07dbd1b3feb570cb84f0d2a6528c
Vanya Rastogi <94480830+VanyaRastogi@users.noreply.github.com>
2022-11-23 15:37:55 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,22 @@
+// var Typed = require('../../../node_modules/typed.js/lib/typed.min.js');
+
+const toggle = document.getElementById('togglelight');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click',function(){
+    this.classList.toggle('bi-moon-fill');
+})
+
+
+
+
+
+
+
+// const typed = new 
+// Typed("#simpleUsage", {
+//     strings: "UI/UX Designing.",
+//     speed: 50,
+//     waitUntilVisible: true,
+//   }).go();
+
